7838e1f1dacd68707cb25c7423248a24ec21de7c
Martin Kearl <Mattline1@live.co.uk>
2022-06-09 15:19:32 +0100

    Grid objects

---
+++ src/Tide.Core/Source/Types/FGrid.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using Tide.Core;
+
+namespace Grim.Core
+{
+    public class FGrid<T> where T : struct
+    {
+        private readonly List<T> data = new List<T>();
+
+        public FGrid(int width, int height)
+        {
+            Height = height;
+            Width = width;
+
+            for (int i = 0; i < Height * Width; i++)
+            {
+                data.Add(default);
+            }
+        }
+
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public T GetAt(FIntVector2 coord)
+        {
+            return data[FGridHelper.Get1DIndex(coord, Width)];
+        }
+
+        public T GetAt(int x, int y)
+        {
+            return data[FGridHelper.Get1DIndex(x, y, Width)];
+        }
+
+        public T GetAt(int i)
+        {
+            return data[i];
+        }
+
+        public void SetAt(FIntVector2 coord, T val)
+        {
+            data[FGridHelper.Get1DIndex(coord, Width)] = val;
+        }
+
+        public void SetAt(int x, int y, T val)
+        {
+            data[FGridHelper.Get1DIndex(x, y, Width)] = val;
+        }
+
+        public void SetAt(int i, T val)
+        {
+            data[i] = val;
+        }
+        
+        public bool IsInBounds(FIntVector2 pos)
+        {
+            return FGridHelper.IsInBounds(pos, Width, Height);
+        }
+
+        public bool IsValidIndex(int i)
+        {
+            return FGridHelper.IsValidIndex(i, Width, Height);
+        }
+    }
+}
---
+++ src/Tide.Core/Source/Types/FGridHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Tide.Core;
+
+namespace Grim.Core
+{
+    public class FGridHelper
+    {
+        public static int Get1DIndex(FIntVector2 coord, int width)
+        {
+            return (coord.y * width) + coord.x;
+        }
+
+        public static int Get1DIndex(int x, int y, int width)
+        {
+            return (y * width) + x;
+        }
+
+        public static FIntVector2 Get2DIndex(int i, int width, int height)
+        {
+            int y = Math.Clamp(i / width, 0, height - 1);
+            int x = i % width;
+            return new FIntVector2(x, y);
+        }
+
+        public static bool IsInBounds(FIntVector2 pos, int width, int height)
+        {
+            return (pos.x >= 0 && pos.x < width) && (pos.y >= 0 && pos.y < height);
+        }
+
+        public static bool IsValidIndex(int i, int width, int height)
+        {
+            return (i >= 0 && i < height * width);
+        }
+    }
+}
