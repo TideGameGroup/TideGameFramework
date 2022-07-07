8cdaa209b5fb77b53c292398c2f34ffc1e0b4841
Martin Kearl <Mattline1@live.co.uk>
2022-07-08 00:59:25 +0100

    FCanvas Refactor

--- src/Tide.XMLSchema/Source/FCanvas.cs
+++ src/Tide.XMLSchema/Source/FCanvas.cs
@@ -30,55 +30,61 @@ namespace Tide.XMLSchema
         text,
         slider,
         tickbox,
-        scrollbar
+        scrollbar,
+        textfield
     }
 
     public struct FCanvas : ISerialisedInstanceData
     {
-        public string               ID;
-        public Rectangle            root;
-        public EWidgetAnchor        anchor;
-        public int[]                parents;
-        public string[]             fonts;
-        public string[]             IDs;
-        public string[]             textures;
-        public string[]             texts;
-        public string[]             tooltips;
-        public Rectangle[]          rectangles;
-        public EWidgetAlignment[]   alignments;
-        public Rectangle[]          sources;
-        public Color[]              colors;
-        public Color[]              highlightColors;
-        public EWidgetType[]        widgetTypes;
+        public EWidgetAlignment[] alignments;
+        public EWidgetAnchor[] anchors;
+        public string[] clickSounds;
+        public Color[] colors;
+        public string[] fonts;
+        public Color[] highlightColors;
+        public string[] hoverSounds;
+        public string ID;
+        public string[] IDs;
+        public int[] parents;
+        public Rectangle[] rectangles;
+        public Rectangle[] sources;
+        public string[] texts;
+        public string[] textures;
+        public string[] tooltips;
+        public EWidgetType[] widgetTypes;
 
         public string Type => "Tide.ACanvas";
 
         public FCanvas DeepCopy()
         {
-            FCanvas deepCopy = new FCanvas();
+            FCanvas deepCopy = new FCanvas
+            {
+                ID = ID,
+                anchors = new EWidgetAnchor[anchors.Length],
+                parents = new int[parents.Length],
+                fonts = new string[fonts.Length],
+                IDs = new string[IDs.Length],
+                textures = new string[textures.Length],
+                texts = new string[texts.Length],
+                tooltips = new string[tooltips.Length],
+                hoverSounds = new string[tooltips.Length],
+                clickSounds = new string[tooltips.Length],
+                rectangles = new Rectangle[rectangles.Length],
+                alignments = new EWidgetAlignment[alignments.Length],
+                sources = new Rectangle[sources.Length],
+                colors = new Color[colors.Length],
+                highlightColors = new Color[highlightColors.Length],
+                widgetTypes = new EWidgetType[widgetTypes.Length]
+            };
 
-            deepCopy.ID = ID;
-            deepCopy.root = root;
-            deepCopy.anchor = anchor;
-
-            deepCopy.parents = new int[parents.Length];
-            deepCopy.fonts = new string[fonts.Length];
-            deepCopy.IDs = new string[IDs.Length];
-            deepCopy.textures = new string[textures.Length];
-            deepCopy.texts = new string[texts.Length];
-            deepCopy.tooltips = new string[tooltips.Length];
-            deepCopy.rectangles = new Rectangle[rectangles.Length];
-            deepCopy.alignments = new EWidgetAlignment[alignments.Length];
-            deepCopy.sources = new Rectangle[sources.Length];
-            deepCopy.colors = new Color[colors.Length];
-            deepCopy.highlightColors = new Color[highlightColors.Length];
-            deepCopy.widgetTypes = new EWidgetType[widgetTypes.Length];
-
+            anchors.CopyTo(deepCopy.anchors, 0);
             parents.CopyTo(deepCopy.parents, 0);
             fonts.CopyTo(deepCopy.fonts, 0);
             IDs.CopyTo(deepCopy.IDs, 0);
             textures.CopyTo(deepCopy.textures, 0);
             texts.CopyTo(deepCopy.texts, 0);
+            hoverSounds.CopyTo(deepCopy.hoverSounds, 0);
+            clickSounds.CopyTo(deepCopy.clickSounds, 0);
             tooltips.CopyTo(deepCopy.tooltips, 0);
             rectangles.CopyTo(deepCopy.rectangles, 0);
             alignments.CopyTo(deepCopy.alignments, 0);
